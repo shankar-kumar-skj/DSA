1473f3363a7f03bd1494236b560648dd207687a4
shankar-kumar-skj <153372713+shankar-kumar-skj@users.noreply.github.com>
2024-11-17 14:13:01 +0530

    Update DAY1_17NOV.cpp

--- DAY1_17NOV.cpp
+++ DAY1_17NOV.cpp
@@ -42,36 +42,36 @@
 
 // TWO SEARCH - O(n) TIME and O(1) SPACE USING 2 TRAVEERSALS
 
-#include<iostream>
-#include<vector>
-using namespace std;
-//function to find the second largest element in the array 
-int getSecondLargest(vector<int>&arr){
-int n=arr.size();
-int largest=-1,secondLargest=-1;
-//finding the largest element element
-for(int i=0;i<n;i++)
-{
-    if(arr[i]>largest)
-    largest =arr[i];
-}
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+// //function to find the second largest element in the array 
+// int getSecondLargest(vector<int>&arr){
+// int n=arr.size();
+// int largest=-1,secondLargest=-1;
+// //finding the largest element element
+// for(int i=0;i<n;i++)
+// {
+//     if(arr[i]>largest)
+//     largest =arr[i];
+// }
 
-//finding the second largest element
-for(int i=0; i<n;i++){
+// //finding the second largest element
+// for(int i=0; i<n;i++){
 
-// Update second largest if the current element is greater than second largest and not equal to the largest 
-if(arr[i]>secondLargest && arr[i]!=largest)
-{
-    secondLargest=arr[i];
-}
-}
-return secondLargest; 
-}
-int main(){
-    vector<int>arr={12,35,1,10,34,1};
-    cout<<getSecondLargest(arr);
-    return 0;
-}
+// // Update second largest if the current element is greater than second largest and not equal to the largest 
+// if(arr[i]>secondLargest && arr[i]!=largest)
+// {
+//     secondLargest=arr[i];
+// }
+// }
+// return secondLargest; 
+// }
+// int main(){
+//     vector<int>arr={12,35,1,10,34,1};
+//     cout<<getSecondLargest(arr);
+//     return 0;
+// }
 
 // ONE PASS SEARCH - O(n) TIME AND O(1) SPACE
 
